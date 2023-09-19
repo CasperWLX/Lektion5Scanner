d9993e5cc0c3917049d6d688f41c2c2f776b7093
CasperWLX <hampuskallberg@gmail.com>
2023-09-19 13:41:38 +0200

    Student class added

--- src/com/hampus/demo/Main.java
+++ src/com/hampus/demo/Main.java
@@ -1,19 +1,27 @@
 package com.hampus.demo;
 
+import com.hampus.demo.modules.Student;
+
 import java.util.Scanner;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        //Objects
         Scanner input = new Scanner(System.in);
+        Student benny = new Student();
 
+
+        //Game
         do
         {
-            System.out.println("Input Number!");
+            System.out.println("Input Benny's age!");
             int result = input.nextInt();
 
             System.out.println("you picked " + result);
+            benny.setAge(result);
+            System.out.println("Benny's age is: " + benny.getAge());
 
         }while(true);
     }
---
+++ src/com/hampus/demo/modules/Student.java
@@ -0,0 +1,18 @@
+package com.hampus.demo.modules;
+
+public class Student
+{
+    private String name;
+    private int age;
+    private boolean isTired;
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+}
